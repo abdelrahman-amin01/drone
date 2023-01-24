513e14afa8a597a816dbfa44320e67b57294dedf
AbdoAmin <abdelrahman.amin01@gmail.com>
2023-01-24 14:54:46 +0200

    adding swager and readme file

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # drone
+-*Open [swagger UI](http://localhost:8080/swagger-ui/) for browsing the API.*<br>
+-*Open [h2 console](http://localhost:8080/h2-console) for browsing memoryDB.*
+- `username: sa` 
+- `password: sa`
+---
+
+-Note that mysql scripts in [schema.sql , data.sql].
---
+++ src/main/java/com/abdelrahman/amin/drone/configration/BeansConfiguration.java
@@ -0,0 +1,27 @@
+package com.abdelrahman.amin.drone.configration;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@EnableScheduling
+public class BeansConfiguration {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
